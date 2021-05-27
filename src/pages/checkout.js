9229b6ccc05e80e6f9abefd4c5e30bcfd97148ae
DeepakSR-12 <deepakdeepakeng@gmail.com>
2021-05-27 13:45:27 +0530

    bug-fix

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -8,7 +8,7 @@ import { useSession } from "next-auth/client";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 
-const stripePromise = loadStripe(process.env.stripe_public_key);
+const stripePromise = loadStripe(`${process.env.stripe_public_key}`);
 
 const Checkout = () => {
   const items = useSelector(selectItems);
